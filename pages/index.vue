f2d897dd46e06ee140fb8bf1aca1daf079571683
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-06-07 16:32:57 -0300

    Project created

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">Funcionando</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
